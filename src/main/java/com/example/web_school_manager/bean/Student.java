1031f7b093504d5cf48ca0087520a353f63cbba7
avp02 <vpetranovskij@gmail.com>
2021-09-09 20:40:37 +0300

    trying test

---
+++ src/main/java/com/example/web_school_manager/bean/Student.java
@@ -0,0 +1,15 @@
+package com.example.web_school_manager.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private String lastName;
+}
