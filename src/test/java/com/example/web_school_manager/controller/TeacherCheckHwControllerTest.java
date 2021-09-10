1cf8dcf20b35aeff53040cb08a5bbc939bec386a
avp02 <vpetranovskij@gmail.com>
2021-09-10 23:52:26 +0300

    tested method deleteHwFrom in TeacherCheckHwController

--- src/test/java/com/example/web_school_manager/controller/TeacherCheckHwControllerTest.java
+++ src/test/java/com/example/web_school_manager/controller/TeacherCheckHwControllerTest.java
@@ -3,7 +3,7 @@ package com.example.web_school_manager.controller;
 import com.example.web_school_manager.bean.HwFromStudent;
 import com.example.web_school_manager.bean.TgUser;
 import com.example.web_school_manager.dao.service.HwFromStudentService;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +13,6 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -28,29 +27,73 @@ class TeacherCheckHwControllerTest {
     @MockBean
     TgUser tgUser;
 
+    private Long id;
+    private HwFromStudent hwFromStudent;
+
+    @BeforeEach
+    public void setup() {
+        hwFromStudent = HwFromStudent.builder()
+                .studentId(1L)
+                .studentName(TgUser.builder().userName("vlad").build())
+                .lessonNumber(1)
+                .hwFromStudent("dasda")
+                .build();
+        id = hwFromStudent.getStudentId();
+    }
+
     @Test
     void checkingHwPage() {
-        when(hwFromStudentService.findAll()).thenReturn(makeExpectedHwFromStudent());
+        when(hwFromStudentService.findAll()).thenReturn(makeExpectedHwFromStudentFromCheckingHwPage());
         ModelAndView actual = teacherCheckHwController.checkingHwPage();
         assertThat(actual).usingRecursiveComparison()
                 .ignoringAllOverriddenEquals()
-                .isEqualTo(getExpectedView());
+                .isEqualTo(getExpectedViewFromCheckingHwPage());
     }
 
-    private List<HwFromStudent> makeExpectedHwFromStudent() {
+    private List<HwFromStudent> makeExpectedHwFromStudentFromCheckingHwPage() {
         return List.of(
                 HwFromStudent.builder()
                         .studentId(1L)
-                        .studentName(TgUser.builder().firstName("voava").build())
+                        .studentName(TgUser.builder().firstName("vova").build())
                         .lessonNumber(1)
                         .hwFromStudent("nikolaevich")
+                        .build(),
+                HwFromStudent.builder()
+                        .studentId(2L)
+                        .studentName(TgUser.builder().firstName("vasia").build())
+                        .lessonNumber(2)
+                        .hwFromStudent("orepri")
                         .build()
         );
     }
 
-    private ModelAndView getExpectedView() {
+    private ModelAndView getExpectedViewFromCheckingHwPage() {
         ModelAndView modelAndView = new ModelAndView("/teacherCheckHw");
         modelAndView.addObject("hwFromStudentList", hwFromStudentService.findAll());
         return modelAndView;
     }
+
+    @Test
+    void deleteHwFrom() {
+        when(hwFromStudentService.findById(id)).thenReturn(hwFromStudent);
+        ModelAndView actual = teacherCheckHwController.deleteHwFrom(id);
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringAllOverriddenEquals()
+                .isEqualTo(getExpectedViewFromDeleteHwFrom(id));
+    }
+
+    private ModelAndView getExpectedViewFromDeleteHwFrom(Long id) {
+        ModelAndView modelAndView = new ModelAndView("deleteHwFromStudent");
+        modelAndView.addObject("hw", hwFromStudentService.findById(id));
+        return modelAndView;
+    }
+
+//    private HwFromStudent exampleFromStudent() {
+//        return HwFromStudent.builder()
+//                .studentId(1L)
+//                .studentName(TgUser.builder().userName("vlad").build())
+//                .lessonNumber(1)
+//                .hwFromStudent("dasda")
+//                .build();
+//    }
 }
