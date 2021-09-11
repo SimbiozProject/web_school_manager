22d62806ad98e3d442ab40c58b1e289acaa93feb
avp02 <vpetranovskij@gmail.com>
2021-09-11 12:57:20 +0300

    tested TeacherCheckHwController thanks to junit tests

--- src/test/java/com/example/web_school_manager/controller/TeacherCheckHwControllerTest.java
+++ src/test/java/com/example/web_school_manager/controller/TeacherCheckHwControllerTest.java
@@ -1,72 +1,165 @@
 package com.example.web_school_manager.controller;
 
-import com.example.web_school_manager.dao.repository.HwFromStudentRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.web_school_manager.bean.HwFromStudent;
+import com.example.web_school_manager.bean.TgUser;
+import com.example.web_school_manager.dao.service.HwFromStudentService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.servlet.ModelAndView;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class TeacherCheckHwControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Autowired
-    HwFromStudentRepository hwFromStudentRepository;
 
     @Autowired
     TeacherCheckHwController teacherCheckHwController;
 
+    @MockBean
+    HwFromStudentService hwFromStudentService;
+
+    @MockBean
+    TgUser tgUser;
+
+    @MockBean
+    HwFromStudent hwFromStudent;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int lessonNumber;
+
+    private List<HwFromStudent> hwFromStudentList;
+
+    @BeforeEach
+    public void setup() {
+        hwFromStudent = HwFromStudent.builder()
+                .studentId(1L)
+                .studentName(TgUser.builder().firstName("vlad").lastName("Gorgun").build())
+                .lessonNumber(1)
+                .hwFromStudent("dasda")
+                .build();
+
+        id = hwFromStudent.getStudentId();
+        firstName = hwFromStudent.getStudentName().getFirstName();
+        lastName = hwFromStudent.getStudentName().getLastName();
+        lessonNumber = hwFromStudent.getLessonNumber();
+
+        hwFromStudentList = List.of(
+                HwFromStudent.builder()
+                        .studentId(1L)
+                        .studentName(TgUser.builder().firstName("vova").build())
+                        .lessonNumber(1)
+                        .hwFromStudent("nikolaevich")
+                        .build(),
+                HwFromStudent.builder()
+                        .studentId(2L)
+                        .studentName(TgUser.builder().firstName("vasia").build())
+                        .lessonNumber(2)
+                        .hwFromStudent("orepri")
+                        .build()
+        );
+    }
+
     @Test
-    void checkingHwPage() throws Exception {
-        //RequestBuilder request = MockMvcRequestBuilders.get("/allHW");
-        //mockMvc.perform(request).andDo(print()).andExpect(status().isOk());
+    void checkingHwPage() {
+        when(hwFromStudentService.findAll()).thenReturn(hwFromStudentList);
+        ModelAndView actual = teacherCheckHwController.checkingHwPage();
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringAllOverriddenEquals()
+                .isEqualTo(getExpectedViewFromCheckingHwPage());
+    }
 
-        RequestBuilder request = MockMvcRequestBuilders.get("/allHw")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(teacherCheckHwController.checkingHwPage()));
-
-        var expected = teacherCheckHwController.checkingHwPage();
-
-        MvcResult result = mockMvc.perform(request)
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(expected), false))
-                .andReturn();
+    private ModelAndView getExpectedViewFromCheckingHwPage() {
+        ModelAndView modelAndView = new ModelAndView("/teacherCheckHw");
+        modelAndView.addObject("hwFromStudentList", hwFromStudentService.findAll());
+        return modelAndView;
     }
 
     @Test
     void deleteHwFrom() {
+        when(hwFromStudentService.findById(id)).thenReturn(hwFromStudent);
+        ModelAndView actual = teacherCheckHwController.deleteHwFrom(id);
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringAllOverriddenEquals()
+                .isEqualTo(getExpectedViewFromDeleteHwFrom(id));
+    }
+
+    private ModelAndView getExpectedViewFromDeleteHwFrom(Long id) {
+        ModelAndView modelAndView = new ModelAndView("/deleteHwFromStudent");
+        modelAndView.addObject("hw", hwFromStudentService.findById(id));
+        return modelAndView;
     }
 
     @Test
     void deleteHw() {
+        doAnswer((i) -> {
+            assertEquals(id, i.getArgument(0));
+            return null;
+        }).when(hwFromStudentService).deleteById(id);
+        ModelAndView actual = teacherCheckHwController.deleteHw(id);
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringAllOverriddenEquals()
+                .isEqualTo(getExpectedViewFromDeleteHw(id));
+    }
+
+    private ModelAndView getExpectedViewFromDeleteHw(Long id) {
+        ModelAndView modelAndView = new ModelAndView("/deleteHwFromStudent");
+        hwFromStudentService.deleteById(id);
+        modelAndView.setViewName("redirect:/teacherCheckHw");
+        return modelAndView;
     }
 
     @Test
     void searchFirstname() {
+        when(hwFromStudentService.findByFirstName(firstName)).thenReturn(hwFromStudentList);
+        ModelAndView actual = teacherCheckHwController.searchFirstname(firstName);
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringAllOverriddenEquals()
+                .isEqualTo(getExpectedViewFromSearchFirstName(firstName));
+    }
+
+    private ModelAndView getExpectedViewFromSearchFirstName(String name) {
+        ModelAndView modelAndView = new ModelAndView("/teacherCheckHw");
+        modelAndView.addObject("hwFromStudentList", hwFromStudentService.findByFirstName(name));
+        return modelAndView;
     }
 
     @Test
     void searchLastName() {
+        when(hwFromStudentService.findByLastName(lastName)).thenReturn(hwFromStudentList);
+        ModelAndView actual = teacherCheckHwController.searchLastName(lastName);
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringAllOverriddenEquals()
+                .isEqualTo(getExpectedViewForSearchLastName(lastName));
+    }
+
+    private ModelAndView getExpectedViewForSearchLastName(String name) {
+        ModelAndView modelAndView = new ModelAndView("/teacherCheckHw");
+        modelAndView.addObject("hwFromStudentList", hwFromStudentService.findByLastName(name));
+        return modelAndView;
     }
 
     @Test
     void searchLesson() {
+        when(hwFromStudentService.findByLesson(lessonNumber)).thenReturn(hwFromStudentList);
+        ModelAndView actual = teacherCheckHwController.searchLesson(lessonNumber);
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringAllOverriddenEquals()
+                .isEqualTo(getExpectedViewForSearchLesson(lessonNumber));
+    }
+
+    private ModelAndView getExpectedViewForSearchLesson(Integer lessonNumber) {
+        ModelAndView modelAndView = new ModelAndView("/teacherCheckHw");
+        modelAndView.addObject("hwFromStudentList", hwFromStudentService.findByLesson(lessonNumber));
+        return modelAndView;
     }
 }
