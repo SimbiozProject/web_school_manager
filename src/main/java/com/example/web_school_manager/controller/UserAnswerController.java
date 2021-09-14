63428126f24bc9f53a0613a53f7f9366bf52df98
avp02 <vpetranovskij@gmail.com>
2021-09-14 18:02:24 +0300

    changed UserAnswerController

--- src/main/java/com/example/web_school_manager/controller/UserAnswerController.java
+++ src/main/java/com/example/web_school_manager/controller/UserAnswerController.java
@@ -1,11 +1,9 @@
 package com.example.web_school_manager.controller;
 
 import com.example.web_school_manager.dao.service.UserAnswerService;
-import io.swagger.models.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,7 +28,7 @@ public class UserAnswerController {
     }
 
     @GetMapping(value = "/searchById/{id}")
-    public ModelAndView searchById(@PathVariable(name = "id") Long id) {
+    public ModelAndView searchById(@PathVariable(value = "id") Long id) {
         ModelAndView modelAndView = new ModelAndView("/allUsersTest");
         modelAndView.addObject("listUserAnswerTable", userAnswerService.findById(id));
         return modelAndView;
